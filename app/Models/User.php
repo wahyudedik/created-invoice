e9003fd77c666ae61883426a8318f7953f6dfb51
wahyudedik <wdedyk@gmail.com>
2024-10-12 15:52:16 +0700

    modified:   README.md 	modified:   app/Models/User.php 	new file:   app/Providers/Filament/AdminPanelProvider.php 	modified:   bootstrap/providers.php 	modified:   composer.json 	modified:   composer.lock 	new file:   public/css/filament/filament/app.css 	new file:   public/css/filament/forms/forms.css 	new file:   public/css/filament/support/support.css 	new file:   public/js/filament/filament/app.js 	new file:   public/js/filament/filament/echo.js 	new file:   public/js/filament/forms/components/color-picker.js 	new file:   public/js/filament/forms/components/date-time-picker.js 	new file:   public/js/filament/forms/components/file-upload.js 	new file:   public/js/filament/forms/components/key-value.js 	new file:   public/js/filament/forms/components/markdown-editor.js 	new file:   public/js/filament/forms/components/rich-editor.js 	new file:   public/js/filament/forms/components/select.js 	new file:   public/js/filament/forms/components/tags-input.js 	new file:   public/js/filament/forms/components/textarea.js 	new file:   public/js/filament/notifications/notifications.js 	new file:   public/js/filament/support/async-alpine.js 	new file:   public/js/filament/support/support.js 	new file:   public/js/filament/tables/components/table.js 	new file:   public/js/filament/widgets/components/chart.js 	new file:   public/js/filament/widgets/components/stats-overview/stat/chart.js 	modified:   routes/web.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,24 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Panel;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+
+    use HasFactory, Notifiable, MustVerifyEmail;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * The attributes that are mass assignable.
